6e1483cb6cccdbe014f962605966c7585f364966
AravindhDeva <aravindhan019@gmail.com>
2022-04-21 22:13:00 +0530

    Branching 2

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,12 @@
+package org.emp;
+
+public class Employee {
+public static void main(String[] args) {
+	System.out.println("emp ID");
+	System.out.println("emp Name");
+	System.out.println("emp Dob");
+	System.out.println("emp Phone");
+	System.out.println("emp Email");
+	System.out.println("emp Address");
+}
+}
